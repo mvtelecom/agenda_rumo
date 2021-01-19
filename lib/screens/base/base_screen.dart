fa29898161ba8675a29c5321e959b2b21647ce63
marcus <mv.telecom20@gmail.com>
2021-01-19 07:21:06 -0300

    Sessão 5 finalizada

---
+++ lib/screens/base/base_screen.dart
@@ -0,0 +1,38 @@
+import 'package:agenda_rumo/common/custom_drawer/custom_drawer.dart';
+import 'package:agenda_rumo/models/page_manager.dart';
+import 'package:agenda_rumo/screens/login/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class BaseScreen extends StatelessWidget {
+  final PageController pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Provider(
+      create: (_) => PageManager(pageController),
+      child: PageView(
+        // controlar alternancia entre as paginas
+        controller: pageController,
+        // não permitir a funçao de rolagem
+        physics: const NeverScrollableScrollPhysics(),
+        // telas do aplicativo
+        children: <Widget>[
+          LoginScreen(),
+          Scaffold(
+            drawer: CustomDrawer(),
+            appBar: AppBar(
+              title: const Text('Home'),
+            ),
+          ),
+          Scaffold(
+            drawer: CustomDrawer(),
+            appBar: AppBar(
+              title: const Text('Home1'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
